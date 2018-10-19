5c8fa0ce8cc3f1c9acd4ce33a816d5848f322f49
thebigez8 <thebigez8@gmail.com>
2018-10-18 21:37:56 -0500

    plot the tour

--- analysis.R
+++ analysis.R
@@ -1,7 +1,7 @@
 
 dat <- read.csv("paired_distances.csv", stringsAsFactors = FALSE)
-parks <- unique(c(dat$Park1, dat$Park2))
-dat.mat <- matrix(0, nrow = 68, ncol = 68, dimnames = list(parks, parks))
+parks <- sort(unique(c(dat$Park1, dat$Park2)))
+dat.mat <- matrix(0, nrow = 73, ncol = 73, dimnames = list(parks, parks))
 for(i in parks)
 {
   for(j in parks)
@@ -13,4 +13,23 @@ for(i in parks)
   }
 }
 
-three_opt(0:67, dat.mat)
+isSymmetric(dat.mat)
+Rcpp::sourceCpp("three_opt.cpp")
+three.opt <- function(distances, tour = 1:ncol(distances), max.swaps = 10000, max.loops = 50)
+{
+  three_opt(tour - 1L, distances, max.swaps, max.loops) + 1L
+}
+
+tour <- three.opt(dat.mat)
+
+park.info <- read_tsv("parks.tsv", col_names = TRUE, col_types = cols())
+
+tour.info <- slice(park.info, match(colnames(dat.mat)[tour], Park))
+
+mn <- map_data("state") %>%
+  filter(region == "minnesota")
+ggplot(mn, aes(x = long, y = lat)) +
+  geom_polygon() +
+  geom_point(data = park.info, color = "red") +
+  geom_path(data = tour.info, color = "red") +
+  coord_equal()
